ab8ade2474ef1141bbcc900e85ac64a8c60117e4
Bartłomiej Mytko <bartlomiej.mytko@gmail.com>
2017-07-04 11:51:27 +0200

    Initial commit

---
+++ src/main/java/week1/Application.java
@@ -0,0 +1,10 @@
+package week1;
+
+/**
+ * Created by mytek on 2017-07-04.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
